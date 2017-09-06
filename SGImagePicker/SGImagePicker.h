1481506d2261ff4682aaa104ecbb9e84310b4eac
Sergey Garazha <garazha.sergey@gmail.com>
2017-09-06 12:04:49 +0300

    base project configuration

---
+++ SGImagePicker/SGImagePicker.h
@@ -0,0 +1,19 @@
+//
+//  SGImagePicker.h
+//  SGImagePicker
+//
+//  Created by Sergey Garazha on 9/6/17.
+//  Copyright © 2017 sergeygarazha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SGImagePicker.
+FOUNDATION_EXPORT double SGImagePickerVersionNumber;
+
+//! Project version string for SGImagePicker.
+FOUNDATION_EXPORT const unsigned char SGImagePickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SGImagePicker/PublicHeader.h>
+
+
